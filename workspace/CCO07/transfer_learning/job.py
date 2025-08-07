ae6bc08e3cc2fcce1708dfeee663f10ae29e7130
Herbert Yao <herbyao@student.ubc.ca>
2025-08-08 03:25:13 +0900

    20250807 update

---
+++ workspace/CCO07/transfer_learning/job.py
@@ -0,0 +1,56 @@
+import scanpy as sc
+import scvi
+
+PREV_DIR = "/home/herbert/PyProjects/cocultured_organ/workspace/CCO06"
+WORK_DIR = "/home/herbert/PyProjects/cocultured_organ/workspace/CCO07"
+
+adata_HVCA = sc.read_h5ad(f"{WORK_DIR}/adata/HVCA/global_object_vasc_atlas_cxg.h5ad")
+adata = sc.read_h5ad(f"{WORK_DIR}/adata/sample_recollect/ALL.h5ad")
+
+adata_HVCA_EN = adata_HVCA[adata_HVCA.obs["cell_type"] == "Endothelial cells"]
+sc.pp.filter_cells(adata_HVCA_EN, min_genes=200)
+sc.pp.filter_genes(adata_HVCA_EN, min_cells=3)
+sc.pp.normalize_total(adata_HVCA_EN, target_sum=1e4)
+sc.pp.log1p(adata_HVCA_EN)
+
+adata_ref = adata_HVCA_EN
+adata_query = adata[adata.obs["cell_type"] == "EN"]
+
+adata_query.obs["organ_uni"] = "Unknown"
+
+# Combine into one AnnData object
+adata_combined = adata_ref.concatenate(
+    adata_query, 
+    batch_key="dataset", 
+    batch_categories=["ref", "query"]
+)
+
+# First, train a base scVI model (initializes weights well)
+scvi.model.SCVI.setup_anndata(
+    adata_combined, 
+    batch_key="dataset", 
+    labels_key="organ_uni", 
+)
+vae = scvi.model.SCVI(adata_combined)
+vae.train(accelerator="cpu", devices=128)
+vae.save(f"{WORK_DIR}/transfer_learning/vae.model")
+
+# Then initialize scANVI from the scVI model
+scvi.model.SCANVI.setup_anndata(
+    adata_combined, 
+    batch_key="dataset", 
+    labels_key="organ_uni", 
+    unlabeled_category="Unknown"
+)
+model = scvi.model.SCANVI.from_scvi_model(vae, unlabeled_category="Unknown")
+model.train(accelerator="cpu", devices=128)
+model.save(f"{WORK_DIR}/transfer_learning/model.model")
+
+adata_combined.obs["predicted_labels"] = model.predict()
+
+# Extract predictions for just the query cells
+adata_query.obs["predicted_labels"] = adata_combined[
+    adata_combined.obs["dataset"] == "query"
+].obs["predicted_labels"]
+
+adata_query.write(f"{WORK_DIR}/transfer_learning/out.h5ad")
--- workspace/CCO07/velocity/scVelo_job.py
+++ workspace/CCO07/velocity/scVelo_job.py
@@ -1,16 +1,32 @@
 import scvelo as scv
 import scanpy as sc
 import matplotlib.pyplot as plt
+import numpy as np
 
 WORK_DIR = "/home/herbert/PyProjects/cocultured_organ/workspace/CCO07"
 
 loom_file_names = ['BVO', 'HO', 'CM3O', 'BHO', 'CM3B']
 adata_file_names = ['VO', 'HO', 'CA', 'VHO', 'VCO']
 
+full_palette = {
+    'CM_0': '#1f77b4',  # blue
+    'CM_1': '#6baed6',  # lighter blue
+    'CM_2': '#08306b',  # dark navy blue
+    'CM_3': '#9ecae1',  # pale blue
+    'EN_H': '#d62728',  # red
+    'EN_V': '#ff9896',  # pinkish red
+    'PR':    '#9467bd',  # purple
+    'FB_V':  '#2ca02c',  # green
+    'FB_H':  '#98df8a',  # light green
+    'SM': '#ff7f0e',     # orange
+    'EP': '#8c564b',     # brown
+    'UN': '#7f7f7f',     # grey (unassigned/unknown)
+}
+
 for i in range(0, 5):
 
     loom_file = f'/home/herbert/PyProjects/cocultured_organ/data/240603/SC160__{loom_file_names[i]}-JW-052224/SC160__{loom_file_names[i]}-JW-052224.loom'
-    ldata = sc.read(loom_file, cache=True)
+    ldata = sc.read(loom_file, cache=False)
     adata = sc.read_h5ad(f'{WORK_DIR}/adata/sample_recollect/{adata_file_names[i]}.h5ad')
     adata = scv.utils.merge(adata, ldata)
 
@@ -18,5 +34,24 @@ for i in range(0, 5):
     scv.pp.moments(adata, n_pcs=50, n_neighbors=50)
     scv.tl.velocity(adata)
     scv.tl.velocity_graph(adata)
-    scv.pl.velocity_embedding_stream(adata, basis='umap', color='cell_type_subcluster', save=f"{WORK_DIR}/velocity/{adata_file_names[i]}.png")
+    palette = [full_palette[cat] for cat in adata.obs['cell_type_subcluster'].cat.categories]
+    scv.pl.velocity_embedding_stream(adata, basis='umap', color='cell_type_subcluster', palette=palette, save=f"{WORK_DIR}/velocity/{adata_file_names[i]}.png")
+
+
+for i in range(0, 5):
+
+    loom_file = f'/home/herbert/PyProjects/cocultured_organ/data/240603/SC160__{loom_file_names[i]}-JW-052224/SC160__{loom_file_names[i]}-JW-052224.loom'
+    ldata = sc.read(loom_file, cache=False)
+    adata = sc.read_h5ad(f'{WORK_DIR}/adata/sample_recollect/{adata_file_names[i]}.h5ad')
+    if np.sum(adata.obs["cell_type"] == "CM") == 0:
+        continue
+    adata = adata[adata.obs["cell_type"] == "CM"]
+    adata = scv.utils.merge(adata, ldata)
+
+    scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+    scv.pp.moments(adata, n_pcs=50, n_neighbors=50)
+    scv.tl.velocity(adata)
+    scv.tl.velocity_graph(adata)
+    palette = [full_palette[cat] for cat in adata.obs['cell_type_subcluster'].cat.categories]
+    scv.pl.velocity_embedding_stream(adata, basis='umap', color='cell_type_subcluster', palette=palette, save=f"{WORK_DIR}/velocity/{adata_file_names[i]}.CM_only.png")
     
